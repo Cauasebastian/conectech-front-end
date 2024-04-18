6d6e1f0c9f19aaf0a380f80575d2a84a08bf94aa
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-04-18 20:39:48 +0000

    LinksHeader component

---
+++ Front-end/src/components/LinksHeader/style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const ItemHeader = styled.a`
+    cursor: pointer;
+    color: #e1e1e1;
+    white-space: nowrap;
+
+    @media(max-width: 686px){
+        display: none;
+    }
+    
+
+    &:hover{
+        color: #fff;
+        
+        
+    }
+    `
